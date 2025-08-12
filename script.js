473f5340d52d6a1e29f731b67804f26900ebe6e6
Ryan Palmer <55644921+jedtherooster@users.noreply.github.com>
2025-08-13 09:57:31 +1200

    Add rounded numbers

--- script.js
+++ script.js
@@ -92,7 +92,7 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    return Math.round((num1 / num2) * 100) / 100;
 }
 
 function operate(num1, num2, operator) {
