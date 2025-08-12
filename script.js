b39752297a02dc9be0a9864f17b37d92168cd0b6
Ryan Palmer <55644921+jedtherooster@users.noreply.github.com>
2025-08-13 08:53:45 +1200

    Most buttons now work

--- script.js
+++ script.js
@@ -2,6 +2,45 @@ let num1 = 0;
 let num2 = 0;
 let operator = "";
 
+const screenText = document.querySelector(".screen h2");
+const clearButton = document.querySelector(".clear");
+
+const operators = document.querySelectorAll(".operator");
+operators.forEach((operator) => {
+    operator.addEventListener("click", () => {
+        operator.classList.add("clicked");
+    })
+});
+
+
+const buttons = document.querySelectorAll(".button");
+buttons.forEach((button) => {
+    if (button.classList.contains("number")) {
+        button.addEventListener("click", () => {
+            console.log(button.id);
+            operators.forEach((operator) => {
+                if (operator.classList.contains("clicked")) {
+                    operator.classList.remove("clicked");
+                    clearScreen();
+                }
+            });
+            screenText.textContent += button.id;
+        });
+    }
+});
+
+
+
+clearButton.addEventListener("click", clearScreen);
+
+
+function clearScreen() {
+    screenText.textContent = "";
+    num1 = 0;
+    num2 = 0;
+    operator = "";
+}
+
 function add(num1, num2) {
     return num1 + num2;
 }
